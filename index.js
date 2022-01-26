ca90e17c8f5ff757e05d755560c221c0d3e6e620
Tanya Rushing <tanyarushing@Tanyas-Air.lan>
2022-01-25 19:05:49 -0500

    first push

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+//window.prompt(weeklyExpenseQuestions[0])
+//let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+//let numberAnswer = parseFloat(stringAnswer)
+//console.log(numberAnswer)
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
